d1da861f784fcb00a1292b634ee06b5cbe009159
Bpslisarenko11 <157765674+Bpslisarenko11@users.noreply.github.com>
2024-02-14 11:33:52 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
-mapboxgl.accessToken = "pk.eyJ1Ijoic3BibGlzYXJlbmtvMTIiLCJhIjoiY2xzMjlodmljMGthcjJrbXRibnRwZ2d3eCJ9.gxylQolcBDuJTH_WfI6MrA";
+mapboxgl.accessToken = "pk.eyJ1Ijoic3BibGlzYXJlbmtvMTIiLCJhIjoiY2xzMjlodmljMGthcjJrbXRibnRwZ2d3eCJ9.gxylQolcBDuJTH_WfI6MrA"; //accesstoken for map style
 
 const map = new mapboxgl.Map({
-    container: "my-map", //map container ID
-    style: "mapbox://styles/spblisarenko12/cls2a6ak501qx01qs4kfu5n97", //style URL
-    center: [-79.41390704282365, 43.64777081498133], //starting position (lon, lat)
-    zoom: 12 //starting zoom
+    container: "my-map", //ID for my-map container
+    style: "mapbox://styles/spblisarenko12/clslj9jrv03xh01p2hed9fldp", //URL for map style
+    center: [-79.41390704282365, 43.64777081498133], //starting position coordinates in longitude and latitude
+    zoom: 12 //starting zoom for the map
 
 });
 
 map.on('load',() => {
 
-    //Add a data source containing GeoJSON data
+    //Data source for three different point types in GeoJSON format
     map.addSource('parks', {
         type: 'geojson',
         data: {
@@ -72,43 +72,44 @@ map.on('load',() => {
     });
 
 
-
-    map.addSource("buildings-data", {
+    //Add an additional source to a GeoJSON file
+    map.addSource("parks-lines", {
         type: "geojson",
-        data: "https://bpslisarenko11.github.io/GGR472-EX5/buildings.geojson",
+        data: "https://bpslisarenko11.github.io/GGR472-LAB2/park_directions1.geojson", // Link to GeoJSON link in GitHub
     
     });
-
+    //Add the GeoJSON link source as a layer
     map.addLayer({
-        "id": "buildings-point",
-        "type": "circle",
-        "source": "buildings-data",
+        "id": "park_directions",
+        "type": "line",
+        "source": "parks-lines",
         "paint": {
-            "circle-radius": 5,
-            "circle-color": "#007cbf",
+            "line-width": 4,
+            "line-color": "#850000",
+            "line-opacity": 0.45 //make the lines semi-transparent
 
         }
     });
-
+    //Add a tileset source from mapbox
     map.addSource("torontoct", {
         "type": "vector",
-        "url": "mapbox://spblisarenko12.0hrkwa3z",
+        "url": "mapbox://spblisarenko12.0hrkwa3z", //link to mapbox tileset
     });
-
+    //Add the mapbox tileset source as a layer
     map.addLayer({
         "id": "census-tract",
         "type": "fill",
         "source": "torontoct",
         "paint": {
-            "fill-color": "#888888",
-            "fill-opacity": 0.4,
-            "fill-outline-color": "black",
+            "fill-color": "#256547",
+            "fill-opacity": 0.45, //make the tileset semi-transparent
+            "fill-outline-color": "green",
 
         },
         "source-layer": "toronto_parks1-5w099v"
 
     },
-        "public-park"
+        "public-park" //make the "public-park" layer load last
     );
     
 });
